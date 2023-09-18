93430aeab686c8128f6e8f60b35689ce1806f982
xaviercorbin <xaviercorbin@outlook.com>
2023-09-18 15:47:30 -0400

    Update config.py

--- Circuit game/config.py
+++ Circuit game/config.py
@@ -1,2 +1,5 @@
 # config.py
 date_start = (2023, 4, 1)
+
+# Aller  chercher les données des ticker et les stocker dans un autre fichier. aller les chercher en ayant un index. 
+#permet d'être plus vite et faire en sorte de ne pas avoir besoind e réseau pour faire des prédictions
